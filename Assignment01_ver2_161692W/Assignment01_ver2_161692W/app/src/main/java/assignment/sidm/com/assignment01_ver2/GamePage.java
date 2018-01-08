e35ba5050d01b456263de485f690d7a8dafcdd71
buttception <spartandevilexdreme@gmail.com>
2018-01-08 11:20:11 +0800

    back space for gmae page

--- Assignment01_ver2_161692W/Assignment01_ver2_161692W/app/src/main/java/assignment/sidm/com/assignment01_ver2/GamePage.java
+++ Assignment01_ver2_161692W/Assignment01_ver2_161692W/app/src/main/java/assignment/sidm/com/assignment01_ver2/GamePage.java
@@ -1,7 +1,9 @@
 package assignment.sidm.com.assignment01_ver2;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.Window;
@@ -29,6 +31,19 @@ public class GamePage extends Activity
         TouchManager.Instance.Update(x,y,event.getAction());
         //Log.d("CLICK","onTouchEvent() called");
         return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Exit")
+                .setMessage("Are you sure you want to exit?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        System.exit(0);
+                    }
+                }).setNegativeButton("No", null).show();
     }
 }
 
