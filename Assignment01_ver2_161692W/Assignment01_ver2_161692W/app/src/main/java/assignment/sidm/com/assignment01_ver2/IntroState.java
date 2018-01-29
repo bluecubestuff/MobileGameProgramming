c35b6b2675ba3159f9740ea9a8f45dd4946a1cfc
buttception <spartandevilexdreme@gmail.com>
2018-01-29 10:22:08 +0800

    instruction thing done

--- Assignment01_ver2_161692W/Assignment01_ver2_161692W/app/src/main/java/assignment/sidm/com/assignment01_ver2/IntroState.java
+++ Assignment01_ver2_161692W/Assignment01_ver2_161692W/app/src/main/java/assignment/sidm/com/assignment01_ver2/IntroState.java
@@ -10,6 +10,9 @@ public class IntroState implements StateBase
 {
     private float timer = 5.0f;
     private Bitmap logo = null;
+    private Bitmap intro = null;
+    private static float rotate = 0;
+    private int screenX, screenY;
 
     @Override
     public String GetName() {
@@ -20,6 +23,9 @@ public class IntroState implements StateBase
     public void OnEnter(SurfaceView _view)
     {
         logo = ResourceManager.Instance.GetBitmap(R.mipmap.ic_launcher);
+        intro = ResourceManager.Instance.GetBitmap(R.drawable.intruct);
+        screenX = _view.getWidth();
+        screenY = _view.getHeight();
     }
 
     @Override
@@ -32,18 +38,23 @@ public class IntroState implements StateBase
     public void Render(Canvas _canvas)
     {
         //EntityManager.Instance.Render(_canvas);
-
-        // Render the logo
+        // Render the log
         Matrix transform = new Matrix();
-        transform.postTranslate(-logo.getWidth() * 0.5f, -logo.getHeight() * 0.5f);
-        transform.postScale(timer, timer);
-        transform.postTranslate(_canvas.getWidth() * 0.5f, _canvas.getHeight()*0.5f);
+        transform.postScale(1.f/intro.getWidth(), 1.f/intro.getHeight());
+        transform.postScale(screenX, screenY);
+        _canvas.drawBitmap(intro, transform, null);
+
+        transform = new Matrix();
+        transform.postTranslate(-logo.getWidth() / 2, -logo.getHeight() / 2);
+        transform.postRotate(rotate);
+        transform.postTranslate(screenX/2, screenY/2);
         _canvas.drawBitmap(logo, transform, null);
     }
 
     @Override
     public void Update(float _dt)
     {
+        rotate += 180 * _dt;
         timer -= _dt;
         if (timer <= 0.0f)
         {
